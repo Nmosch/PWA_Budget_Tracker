f472b00d54638d10fa979078c0396d0f896f0417
NMosch <nmosch@gmail.com>
2020-07-05 19:28:20 -0700

    succesfully created ObjectStore

--- public/db.js
+++ public/db.js
@@ -1,10 +1,16 @@
 const dbRequest = indexedDB.open("transactions_db", 1);
 
+dbRequest.onupgradeneeded = event => {
+    const db = event.target.result;
+    console.log(`Initializing db...`);
+    db.createObjectStore("transactions", { autoIncrement: true });
+};
+
 dbRequest.onsuccess = event => {
-    console.log("Successfully opened connection to IndexedDB")
+    const db = event.target.result;
+    console.log(`Successfully opened connection to ${db.name}`);
 };
 
 dbRequest.onerror = event => {
-    console.log("Error opening connecttion to IndexedDB")
-
-}
+    console.log(`Error opening connecttion to IndexedDB: ${event.target.error}`);
+};
